232d525eebe0ea445678b1ef60e7718eb1b47c80
Meonako <dubi.db28@gmail.com>
2024-02-12 08:54:41 +0700

    Removed: `linux-musl` target

--- build.zig
+++ build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 const targets: []const std.Target.Query = &.{
-    .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
     .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
     .{ .cpu_arch = .x86_64, .os_tag = .windows },
 };
